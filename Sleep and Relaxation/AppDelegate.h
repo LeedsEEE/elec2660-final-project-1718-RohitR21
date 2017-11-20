353a8fdee73997e880d8418eb127525950d066f2
Rohit Rajkotia [el16rr] <el16rr@eentpc64.leeds.ac.uk>
2017-11-20 10:10:40 +0000

    Initial Commit

---
+++ Sleep and Relaxation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sleep and Relaxation
+//
+//  Created by Rohit Rajkotia [el16rr] on 20/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sleep and Relaxation/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Sleep and Relaxation
+//
+//  Created by Rohit Rajkotia [el16rr] on 20/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
